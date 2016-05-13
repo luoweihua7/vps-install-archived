1a5d5ec0a6e36351911d9fd37626951cb0586221
Larify <luoweihua7@users.noreply.github.com>
2016-05-13 23:04:03 +0800

    Create pptp.sh
    first commit

---
+++ pptp.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function installVPN(){
+	echo "begin to install VPN services";
+	#check wether vps suppot ppp and tun
+	
+	yum remove -y pptpd ppp
+	iptables --flush POSTROUTING --table nat
+	iptables --flush FORWARD
+	rm -rf /etc/pptpd.conf
+	rm -rf /etc/ppp
+
+	rpm -Uvh http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
+	yum install -y pptpd
+
+	mknod /dev/ppp c 108 0 
+	echo 1 > /proc/sys/net/ipv4/ip_forward 
+	echo "mknod /dev/ppp c 108 0" >> /etc/rc.local
+	echo "echo 1 > /proc/sys/net/ipv4/ip_forward" >> /etc/rc.local
+	echo "localip 172.16.36.1" >> /etc/pptpd.conf
+	echo "remoteip 172.16.36.2-254" >> /etc/pptpd.conf
+	echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd
+	echo "ms-dns 8.8.4.4" >> /etc/ppp/options.pptpd
+
+	pass=`openssl rand 6 -base64`
+	if [ "$1" != "" ]
+	then pass=$1
+	fi
+
+	echo "vpn pptpd ${pass} *" >> /etc/ppp/chap-secrets
+
+	iptables -t nat -A POSTROUTING -s 172.16.36.0/24 -j SNAT --to-source `ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 'NR==1 { print $1}'`
+	iptables -A FORWARD -p tcp --syn -s 172.16.36.0/24 -j TCPMSS --set-mss 1356
+	service iptables save
+
+	chkconfig iptables on
+	chkconfig pptpd on
+
+	service iptables start
+	service pptpd start
+
+	echo "VPN service is installed, your VPN username is vpn, VPN password is ${pass}"
+	
+}
+
+function repaireVPN(){
+	echo "begin to repaire VPN";
+	mknod /dev/ppp c 108 0
+	service iptables restart
+	service pptpd start
+}
+
+function addVPNuser(){
+	echo "input user name:"
+	read username
+	echo "input password:"
+	read userpassword
+	echo "${username} pptpd ${userpassword} *" >> /etc/ppp/chap-secrets
+	service iptables restart
+	service pptpd start
+}
+
+echo "which do you want to?input the number."
+echo "1. install VPN service"
+echo "2. repaire VPN service"
+echo "3. add VPN user"
+read num
+
+case "$num" in
+[1] ) (installVPN);;
+[2] ) (repaireVPN);;
+[3] ) (addVPNuser);;
+*) echo "bye bye";;
+esac
