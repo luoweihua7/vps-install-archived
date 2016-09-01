ac9bcb9da866cba5a6fa99e38430db2dcd19cf44
luoweihua7 <luoweihua7@users.noreply.github.com>
2016-09-01 22:16:23 +0800

    Update install_lighttpd.sh

--- lighttpd/install_lighttpd.sh
+++ lighttpd/install_lighttpd.sh
@@ -1,6 +1,8 @@
-
 wget --no-check-certificate https://github.com/luoweihua7/vps-install/raw/master/lighttpd/lighttpd.tar.gz -O /tmp/lighttpd.tar.gz
-wget --no-check-certificate 
 tar zxvf /tmp/lighttpd.tar.gz -C /koolshare/
-rm -rf /tmp/lighttpd.tar.gz
+
+wget --no-check-certificate https://github.com/luoweihua7/vps-install/raw/master/lighttpd/lighttpd_check.sh -O /koolshare/scripts/lighttpd_check.sh
 echo "sh /koolshare/scripts/lighttpd_check.sh" >> /jffs/scripts/wan-start
+sh /koolshare/scripts/lighttpd_check.sh
+
+rm -rf /tmp/lighttpd.tar.gz
