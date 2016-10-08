faf6050613f4d7af73325d9c28350701e1ba9955
luoweihua7 <luoweihua7@users.noreply.github.com>
2016-10-08 22:08:34 +0800

    Update shadowsocks-over-kcp.sh

--- shadowsocks/shadowsocks-over-kcp.sh
+++ shadowsocks/shadowsocks-over-kcp.sh
@@ -181,9 +181,9 @@ function add_service() {
 
     add_firewall $kcpport
 
-    # nohup $default_kcp_path -l :$kcpport -t 127.0.0.1:$ssport --crypt none --mtu 1200 --nocomp --mode $kcpmode --dscp 46 & ss-server -s 0.0.0.0 -p $ssport -k $sspwd -m $ssencrypt -u &
+    nohup $default_kcp_path -l :$kcpport -t 127.0.0.1:$ssport --crypt none --mtu 1200 --nocomp --mode $kcpmode --dscp 46 & ss-server -s 0.0.0.0 -p $ssport -k $sspwd -m $ssencrypt -u &
 
-    # echo "nohup $default_kcp_path -l :$kcpport -t 127.0.0.1:$ssport --crypt none --mtu 1200 --nocomp --mode $kcpmode --dscp 46 & ss-server -s 0.0.0.0 -p $ssport -k $sspwd -m $ssencrypt -u &" >> /etc/rc.local
+    echo "nohup $default_kcp_path -l :$kcpport -t 127.0.0.1:$ssport --crypt none --mtu 1200 --nocomp --mode $kcpmode --dscp 46 & ss-server -s 0.0.0.0 -p $ssport -k $sspwd -m $ssencrypt -u &" >> /etc/rc.local
 
     echo ""
     echo -e "Server IP is\t\t\t\033[32m$localip\033[0m"
