4e7236ee58e147757944701660933a29fa6dc60c
Larify <luowehua7@gmail.com>
2021-01-19 17:37:35 +0800

    chore: 删除垃圾文件

--- onekey/tencent.sh
+++ onekey/tencent.sh
@@ -75,20 +75,18 @@ net.ipv4.tcp_congestion_control = hybla" >> /etc/sysctl.conf
 }
 
 update_wget() {
+    echo "Installing require dependents, please wait..."
     yum install -y openssl-devel openssl
-    #wget_ver="wget-1.20.3"
-    #wget https://ftp.gnu.org/gnu/wget/${wget_ver}.tar.gz
-    # tar xvf ${wget_ver}.tar.gz
-    # cd ${wget_ver}
 
     wget http://mirrors.ustc.edu.cn/gnu/wget/wget-latest.tar.gz
     mkdir /tmp/wget-latest
     tar xvf wget-latest.tar.gz -C /tmp/wget-latest --strip-components 1 >> /dev/null 2>&1
     cd /tmp/wget-latest
-    ./configure --prefix=/usr --sysconfdir=/etc --with-ssl=openssl | grep -a3 "err|warn"
-    make && make install | grep -a3 "err|warn"
+    ./configure --prefix=/usr --sysconfdir=/etc --with-ssl=openssl
+    make && make install
     cd ~/
     rm -rf /tmp/wget-latest
+    rm -rf wget-latest.tar.gz
 }
 
 setup() {
