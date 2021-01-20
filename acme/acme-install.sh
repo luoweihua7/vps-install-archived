8a1a7a9c650fe3d7dbbfac236d98aaa4f52cc60d
Larify <luowehua7@gmail.com>
2021-01-20 16:46:59 +0800

    feat: acme证书服务

--- acme/acme-install.sh
+++ acme/acme-install.sh
@@ -17,47 +17,91 @@ ERROR="${red}[ERROR]${plain}"
 SUCCESS="${green}[SUCCESS]${plain}"
 READ_INFO=$'\e[31m[INFO]\e[0m'
 
-NGINX_CONF_DIR="/etc/nginx/cert"
+NGINX_CERT_DIR="/etc/nginx/certificates"
+DNS_SERVICE=""
 
 acme_install() {
-  echo -e "Installing Let's Encrypt SSL certificate..."
+  echo -e "${INFO} Installing acme.sh service..."
   curl  https://get.acme.sh | sh
+  echo -e "${SUCCESS} acme.sh service install success."
 
-  # Input Aliyun AccessKey
-  echo ""
-  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input Aliyun AccessKey ID: " access_key
-  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input Aliyun Access Key Secret: " access_secret
-  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input your domain (eg. domain.com, NOT subdomain): " root_domain
-  echo ""
+  main
+}
 
-  export Ali_Key="${access_key}"
-  export Ali_Secret="${access_secret}"
+acme_configure_check() {
+  if [ "${DNS_SERVICE}" == "dns_ali" ]; then
+    # Aliyun
+    ali_conf=`grep "SAVED_Ali_Key" ~/.acme.sh/account.conf | wc -l`
+    if [ ${ali_conf} -ne 0 ]; then
+      echo -e "${INFO} Aliyun already configured."
+    else
+      echo -e "${INFO} Please configure Aliyun secret first."
+      # Aliyun AccessKey
+      echo ""
+      echo "Aliyun AccessKey page: https://ram.console.aliyun.com/manage/ak"
+      echo ""
+      stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input Aliyun AccessKey ID: " ali_key
+      stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input Aliyun AccessKey Secret: " ali_secret
+      echo ""
 
-  ~/.acme.sh/acme.sh --issue --dns dns_ali -d ${root_domain} -d *.${root_domain} --yes-I-know-dns-manual-mode-enough-go-ahead-please
+      export Ali_Key="${ali_key}"
+      export Ali_Secret="${ali_secret}"
+    fi
+  elif [ "${DNS_SERVICE}" == "dns_dp" ]; then
+    # DNSPod
+    dp_conf=`grep "SAVED_DP_Id" ~/.acme.sh/account.conf | wc -l`
+    if [ ${dp_conf} -ne 0 ]; then
+      echo -e "${INFO} DNSPod already configured."
+    else
+      echo -e "${INFO} Please configure DNSPod secret first."
+      # DNSPod API
+      echo ""
+      echo "DNSPod API Token page: https://console.dnspod.cn/account/token"
+      echo ""
+      stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input DNSPod API ID: " dp_id
+      stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input DNSPod API Token: " dp_key
+      echo ""
 
-  # Check certificate exist
-  if [ ! -f ~/.acme.sh/${root_domain}/${root_domain}.cer ]; then
-    echo -e "${ERROR} Generate certificate failure!!!"
-    exit 3
+      export DP_Id="${dp_id}"
+      export DP_Key="${dp_key}"
+    fi
   else
-    mkdir -p ${NGINX_CONF_DIR}
-
-    # Install certificate to target folder
-    ~/.acme.sh/acme.sh --installcert -d ${root_domain} --fullchainpath ${NGINX_CONF_DIR}/${root_domain}.crt --keypath ${NGINX_CONF_DIR}/${root_domain}.key --reloadcmd "service nginx force-reload"
+      echo -e "${ERROR} Unsupported service, please try again."
+      exit 1
   fi
 }
 
+choose_service() {
+  # Choose dns service
+  echo ""
+  echo -e "Which one dns do you want to configure?"
+  echo "1. Aliyun"
+  echo "2. DNSPod"
+  echo ""
+  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Input the number and press enter. (Press any other key to exit) " num
+
+  case "${num}" in
+    [1] )
+      DNS_SERVICE="dns_ali"
+    ;;
+    [2] )
+      DNS_SERVICE="dns_dp"
+    ;;
+    *) ;;
+  esac
+}
+
 acme_add_domain() {
-  echo -e "Installing Let's Encrypt SSL certificate..."
-  curl  https://get.acme.sh | sh
+  echo -e "Add new domain Let's Encrypt SSL certificate..."
 
   # Input Aliyun AccessKey
   echo ""
-  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input your domain (eg. domain.com): " add_domain
+  stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input your domain (eg. domain.com, NOT subdomain): " add_domain
   stty erase '^H' && stty erase ^? && read -p "${READ_INFO} Please input certificate folder (eg. /etc/nginx/cert): " ng_cert_dir
   echo ""
 
-  ~/.acme.sh/acme.sh --issue --dns dns_ali -d ${add_domain}
+  # regist wildcard domain
+  ~/.acme.sh/acme.sh --issue --dns ${DNS_SERVICE} -d ${add_domain} -d *.${add_domain} --yes-I-know-dns-manual-mode-enough-go-ahead-please --force
 
   # Check certificate exist
   if [ ! -f ~/.acme.sh/${add_domain}/${add_domain}.cer ]; then
@@ -82,7 +126,11 @@ main() {
 
   case "${num}" in
     [1] ) (acme_install);;
-    [2] ) (acme_add_domain);;
+    [2] )
+      choose_service
+      acme_configure_check
+      acme_add_domain
+    ;;
     *) echo "Bye~~~";;
   esac
 }
